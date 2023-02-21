71be90820b503e48654566b3e0228c1a4ec524d9
maksimuspiter <maksimuspiter@gmail.com>
2023-02-21 09:52:52 +0300

    add filters drm

--- books/store/tests/test_api.py
+++ books/store/tests/test_api.py
@@ -4,14 +4,35 @@ from rest_framework.test import APITestCase
 from store.models import Book
 from store.serializer import BookSerializer
 
-class BooksApiTestCase(APITestCase):
-    def test_get(self):
-        book_1 = Book.objects.create(name='Test Book1', price=25)
-        book_2 = Book.objects.create(name='Test Book2', price=55)
 
+class BooksApiTestCase(APITestCase):
+
+    def setUp(self):
+        self.book_1 = Book.objects.create(name='Test book 1', price=25,
+                                          author_name='Author 1')
+        self.book_2 = Book.objects.create(name='Test book 2', price=55,
+                                          author_name='Author 5')
+        self.book_3 = Book.objects.create(name='Test book Author 1', price=55,
+                                          author_name='Author 2')
+
+    def test_get(self):
         url = reverse('book-list')
         response = self.client.get(url)
-        serializer_data = BookSerializer([book_1, book_2], many=True).data
+        serializer_data = BookSerializer([self.book_1, self.book_2, self.book_3], many=True).data
 
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_filter(self):
+        url = reverse('book-list')
+        response = self.client.get(url)
+        serializer_data = BookSerializer([self.book_1, self.book_2, self.book_3], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+    def test_get_search(self):
+        url = reverse('book-list')
+        response = self.client.get(url, data={'search': 'Author 1'})
+        serializer_data = BookSerializer([self.book_1, self.book_3], many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
--- books/store/views.py
+++ books/store/views.py
@@ -1,4 +1,7 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
+
 
 from store.models import Book
 from store.serializer import BookSerializer
@@ -7,3 +10,10 @@ from store.serializer import BookSerializer
 class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    #filter_fields = ['price']
+
+    filterset_fields = ['price']
+    search_fields = ['name', 'author_name']
+    ordering_fields = ['price', 'author_name']
+
