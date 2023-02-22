e7f5389de571a60ebf6051497cfe959905c67483
maksimuspiter <maksimuspiter@gmail.com>
2023-02-22 18:38:03 +0300

    add serializers's method

--- drfsite/women/views.py
+++ drfsite/women/views.py
@@ -21,12 +21,43 @@ class WomenAPIView(APIView):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
 
-        post_new = Women.objects.create(title=request.data['title'],
-                                        content=request.data['content'],
-                                        cat_id=request.data['cat_id'])
+        # post_new = Women.objects.create(title=request.data['title'],
+        #                                 content=request.data['content'],
+        #                                 cat_id=request.data['cat_id'])
+        serializer.save()
 
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({'post': serializer.data})
 
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'Method PUT not allowed'})
 
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({'error': 'Object does not exist'})
 
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response({'post': serializer.data})
 
+    def delete(self, request, pk=None):
+
+        # record = Women.obj
+        # pk = kwargs.get('pk', None)
+        # if not pk:
+        #     return Response({'error': 'Method DELETE not allowed'})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+            instance.delete()
+        except:
+            return Response({'error': 'Object does not exist'})
+
+        # serializer = WomenSerializer(data=request.data, instance=instance)
+        # serializer.is_valid(raise_exception=True)
+        # serializer.save()
+
+        return Response({'post': "delete post " + str(id)})
